bb6ef964c193f0e347369b3f1381ac8085aa4736
shuninamarina <marinashunina03@gmail.com>
2023-01-09 14:55:52 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <li class = "gallery__item">
+    <a class = "gallery__link" href= "${original}">
+    <img  
+    class = "gallery__image" 
+    data-source = "${original}" 
+    src = ${preview} 
+    alt = "${description}">
+    </a>
+    </li>
+    `;
+  })
+  .join("");
+
+gallery.addEventListener("click", openModalWindow);
+
+function openModalWindow(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+
+  const imageModal = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="auto">`,
+    {
+      onShow: () => window.addEventListener("keydown", onEscape),
+      onClose: () => window.removeEventListener("keydown", onEscape),
+    }
+  );
+
+  function onEscape(event) {
+    if (event.code === "Escape") {
+      imageModal.close();
+    }
+  }
+  imageModal.show();
+}
